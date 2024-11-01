7302a46beec7d85656d56afe1062056bd054720f
Daniel Zapata <danielfelipe.zapata@bbva.com>
2024-11-01 13:19:47 -0500

    se agregan modificaciones para que funcione el servicio y los modulos

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { VehiculosModule } from './vehiculos/vehiculos.module'; // Importa el módulo de vehículos
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // Importa HttpClientModule
+
+
+
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+    AppComponent,
+    // Otros componentes de AppModule, si los tienes
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    VehiculosModule // Añade el módulo aquí
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/vehiculos/vehiculos.module.ts
+++ src/app/vehiculos/vehiculos.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ListarVehiculoComponent } from './listar-vehiculo/listar-vehiculo.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListarVehiculoComponent // Declara el componente aquí
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ListarVehiculoComponent // Exporta el componente para usarlo en otros módulos
   ]
 })
 export class VehiculosModule { }
